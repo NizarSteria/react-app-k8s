e46e43ea6e089083a4ba0403d99597e0be80fca5
Nizat <aouissaoui.nizar@gmail.com>
2019-02-04 14:28:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <p> Demo application with React, Docker and K8S
+          </p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
